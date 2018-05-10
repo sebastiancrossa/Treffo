3eca83877511aaf355c8dad4fced99a33ab67a69
Sebastian C <crossasebastian@gmail.com>
2018-05-09 21:05:15 -0600

    Set table view

--- Treffo/Controller/GoalsVC.swift
+++ Treffo/Controller/GoalsVC.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addGoalButtonWasPressed(_ sender: Any) {
+        
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
