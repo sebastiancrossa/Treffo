5f716a5b41fb1c057aa3bba39e586158eebcd949
Sebastian C <crossasebastian@gmail.com>
2018-05-09 20:48:18 -0600

    Added assets. Started work on homescreen

--- Treffo/Controller/GoalsVC.swift
+++ Treffo/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  Treffo
 //
 //  Created by Sebastian Crossa on 5/9/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
 }
 
