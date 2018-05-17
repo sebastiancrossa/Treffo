e77c4482fc710901051aa4c37b585ba12362970a
Sebastian C <crossasebastian@gmail.com>
2018-05-17 13:24:48 -0600

    Created prototype cell

---
+++ Treffo/View/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  Treffo
+//
+//  Created by Sebastian Crossa on 5/17/18.
+//  Copyright © 2018 Sebastian Crossa. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
